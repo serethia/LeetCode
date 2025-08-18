1730a9ca096b5f53678ae2249eaf507207d2ce83
Sewon Oum <137035446+serethia@users.noreply.github.com>
2025-08-18 13:27:33 +0900

    Time: 1 ms (99.87%), Space: 53.2 MB (49.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,7 +3,18 @@ import java.io.*;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int now = 0;
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (cnt == 0) {
+                now = nums[i];
+            }
+            if (now == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        return now;
     }
 }
