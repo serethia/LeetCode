3f39812e7fa8441d23dddbe3c9636cf656f9946f
Sewon Oum <137035446+serethia@users.noreply.github.com>
2025-08-18 13:30:17 +0900

    Time: 3 ms (47.6%), Space: 45.7 MB (56.07%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > curr) {
+                return false;
+            }
+            curr = Math.max(curr, nums[i] + i);
+        }
+        return true;
+    }
+}
